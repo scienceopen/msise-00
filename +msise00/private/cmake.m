2119b52c0b4eaf16663d4fcffc65822d56038c09
scivision <scivision@users.noreply.github.com>
2024-12-19 18:01:43 -0500

    ci: add windows
    fix annotations for Python 3.9
    
    ci:matlab: add windows

--- +msise00/private/cmake.m
+++ +msise00/private/cmake.m
@@ -13,6 +13,10 @@ fix_macos()
 assert(system("cmake --version") == 0, 'CMake not found')
 
 cmd = "cmake -B" + build_dir + " -S" + src_dir;
+if ispc && isempty(getenv("CMAKE_GENERATOR"))
+  cmd = cmd + " -G MinGW Makefiles";
+end
+
 assert(system(cmd) == 0, 'error building with CMake')
 
 cmd = "cmake --build " + build_dir;
--- src/msise00/__init__.py
+++ src/msise00/__init__.py
@@ -1,4 +1,4 @@
 from .base import run, rungtd1d
 from .timeutils import todatetime, todt64
 
-__version__ = "1.11.0"
+__version__ = "1.11.1"
